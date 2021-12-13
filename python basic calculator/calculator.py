ef435a2f17d57144aa609685468f90b748b57f41
João Pedro Costa <joaopedro08costa@hotmail.com>
2021-12-13 12:42:33 -0300

    code in portuguese

--- python basic calculator/calculator.py
+++ python basic calculator/calculator.py
@@ -1,6 +1,82 @@
-n1 = int(input('Type the first number: '))
-n2 = int(input('Type the second number: '))
+print('1 - Soma')
+print('2 - Subtração')
+print('3 - Multiplicação')
+print('4 - Divisão')
+print('5 - Sair\n')
 
-res = n1 + n2
+op = int(input('Escolha uma operação para fazer: '))
+while op != 5:
 
-print('The result is: '.format(res))
+  if op == 1:
+    n1 = int(input('\nDigite um número: '))
+    n2 = int(input('Digite outro número: '))
+
+    res = n1 + n2
+    print('\nO resultado da soma é: {}\n'.format(res))
+
+
+
+    print('1 - Soma')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+    print('5 - Sair\n')
+
+    op = int(input('Escolha uma operação para fazer: '))
+
+  elif op == 2:
+    n1 = int(input('\nDigite um número: '))
+    n2 = int(input('Digite outro número: '))
+
+    res = n1 - n2
+    print('\nO resultado da subtração é: {}\n'.format(res))
+
+    print('1 - Soma')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+    print('5 - Sair\n')
+
+    op = int(input('Escolha uma operação para fazer: '))
+
+  elif op == 3:
+    n1 = int(input('\nDigite um número: '))
+    n2 = int(input('Digite outro número: '))
+
+    res = n1 * n2
+    print('\nO resultado da multiplicação é: {}\n'.format(res))
+
+    print('1 - Soma')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+    print('5 - Sair\n')
+
+    op = int(input('Escolha uma operação para fazer: '))
+
+  elif op == 4:
+    n1 = int(input('\nDigite um número: '))
+    n2 = int(input('Digite outro número: '))
+
+    res = n1 / n2
+    print('\nO resultado da divisão2 é: {:.0f}\n'.format(res))
+
+    print('1 - Soma')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+    print('5 - Sair\n')
+
+    op = int(input('Escolha uma operação para fazer: '))
+
+  elif op <= 0 or op >= 6:
+    print('OPÇÃO INVALIDA!\n')
+    print('1 - Soma')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+    print('5 - Sair\n')
+
+    op = int(input('Escolha uma operação para fazer: '))
+    
+print('FIM!')
