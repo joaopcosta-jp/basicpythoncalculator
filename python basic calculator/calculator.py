f74b88a6efd4809f6a1c89476c902476a122c680
joaopcosta-jp <joaopedro08costa@hotmail.com>
2021-12-13 04:20:16 -0300

    testing

---
+++ python basic calculator/calculator.py
@@ -0,0 +1,6 @@
+n1 = int(input('Type the first number: '))
+n2 = int(input('Type the second number: '))
+
+res = n1 + n2
+
+print('The result is: '.format(res))
---
+++ python basic calculator/readme.md
@@ -0,0 +1,2 @@
+Trying to do a basic calculator with python.
+Tentando fazer uma calculadora simples em python.
